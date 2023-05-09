1e81879dba1f2ef7adbeeb654f1711ac96a20a9b
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-05-09 17:35:44 -0300

    fix(backend): player update by name and tag

--- back-end/src/players/players.repository.ts
+++ back-end/src/players/players.repository.ts
@@ -6,15 +6,17 @@ import { CreatePlayerDTO } from './dtos/create-player-dto';
 export class PlayersRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  create(body: CreatePlayerDTO) {
+  create({ name, tag, rankId }: CreatePlayerDTO) {
     return this.prisma.player.upsert({
-      where: { name_tag: body },
-      create: { ...body },
-      update: { ...body },
+      where: { name_tag: { name, tag } },
+      create: { name, tag, rankId },
+      update: { name, tag, rankId },
     });
   }
 
   findAll() {
-    return this.prisma.player.findMany();
+    return this.prisma.player.findMany({
+      include: { Rank: { select: { id: true, name: true, image: true } } },
+    });
   }
 }
